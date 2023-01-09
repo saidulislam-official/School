c0b6e8c5b8784335f4e83a43d19b615334e76028
saidulislam-official <saidulislam.own@gmail.com>
2023-01-09 19:20:09 +0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "School",
+
+    'summary': """
+        School Data Create
+        """,
+
+    'description': """
+      School
+    """,
+
+    'author': "Saidul Islam",
+    'version': '15.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+
+    ],
+    # only loaded in demonstration mode
+
+}
